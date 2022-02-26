ec06fcc65236136fb5fa3ee68741906187e4e8c0
FATİH YILDIRIM <21792018+fatihyldrim@users.noreply.github.com>
2022-02-26 17:40:51 +0300

    className error fixed

--- components/Hero.js
+++ components/Hero.js
@@ -39,7 +39,7 @@ const Hero = () => {
             </div>
 
             <div className="flex flex-col mb-2">
-              <div class=" relative ">
+              <div className=" relative ">
               <input type="text" id="rounded-email" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Your Email"/>
               </div>
             </div>
